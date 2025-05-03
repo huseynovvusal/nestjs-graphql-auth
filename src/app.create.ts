59cb37eb5f79277d400232e7785c347255c4547e
Vusal Huseynov <vslh333222111@gmail.com>
2025-05-03 14:38:20 +0400

    feat: add global validation pipe to application and enhance CreateUserInput with validation decorators

--- src/app.create.ts
+++ src/app.create.ts
@@ -1,6 +1,7 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { EntityNotFoundFilter } from './entity-not-found/entity-not-found.filter';
 
 export function createApp(app: INestApplication): void {
   app.useGlobalFilters(new EntityNotFoundFilter());
+  app.useGlobalPipes(new ValidationPipe());
 }
--- src/user/dto/create-user.input.ts
+++ src/user/dto/create-user.input.ts
@@ -1,10 +1,14 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsString } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
+  @IsString()
   @Field()
   username: string;
 
+  @IsString()
+  @IsEmail()
   @Field()
   email: string;
 }
